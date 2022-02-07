aece623c0e9231fa64794672c3f7c714cdb3a658
josephStuff <mrwatsonjoe28@yahoo.com>
2022-02-07 17:31:22 -0500

    Add project files.

---
+++ AtlasBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AtlasBlog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtlasBlog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BlogUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ AtlasBlog/Models/BlogUser.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace AtlasBlog.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        public string FirstName { get; set; } = "";
+
+        public string LastName { get; set; } = "";
+
+        public string? Displayname { get; set; }
+    }
+
+}
