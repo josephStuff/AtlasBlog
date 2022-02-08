cec35bed95b0d366792c47d70b82ef6d697c88be
josephStuff <mrwatsonjoe28@yahoo.com>
2022-02-08 15:47:22 -0500

    updated more stuff

--- AtlasBlog/Program.cs
+++ AtlasBlog/Program.cs
@@ -1,12 +1,15 @@
 using AtlasBlog.Data;
 using AtlasBlog.Models;
+using AtlasBlog.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var connectinoString = ConnectionService.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
@@ -23,7 +26,19 @@ builder.Services.AddIdentity<BlogUser, IdentityRole>(options => options.SignIn.R
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddTransient<DataService>();
+
+// ---------------------------- LINE OF DEMARCATION ---------------
 var app = builder.Build();
+
+// ------------------------ WHEN CALLING A SERVICE FROM THIS MIDDLEWARE WE NEED AN INSTANCE OF IServiceScope
+var scope = app.Services.CreateScope();
+var dataService = scope.ServiceProvider.GetRequiredService<DataService>();
+await dataService.SetupDb();
+
+
+// --------- await scope.ServiceProvider.GetRequiredService<DataService>().SetupDbAsync();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,6 +51,10 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ AtlasBlog/Services/DataService.cs
@@ -0,0 +1,23 @@
+﻿using AtlasBlog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtlasBlog.Services
+{
+    public class DataService
+    {
+        // ------------ CALLING A METHOD OR INSTRUCTION THAT EXECUTES THE MIGRATIONS INSTRUCTIONS ----------
+        readonly ApplicationDbContext _dbContext;
+
+        public DataService(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+
+        public async Task SetupDb()
+        {
+            // ---------------  _RUN THE MIGRATIONS ASYNC --------------
+            await _dbContext.Database.MigrateAsync();
+        }
+    }
+}
