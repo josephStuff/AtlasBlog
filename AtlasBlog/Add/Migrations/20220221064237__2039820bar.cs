a88a8014199ee71af000a0ee7c87b4c9dec28019
josephStuff <mrwatsonjoe28@yahoo.com>
2022-02-21 10:46:56 -0500

    added tag model and many to many relationship.  scaffolded from tag model

---
+++ AtlasBlog/Add/Migrations/20220221064237__2039820bar.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtlasBlog.Add.Migrations
+{
+    public partial class _2039820bar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AtlasBlog/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtlasBlog.Models
+{
+    public class Tag
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; } = "";
+        public string Description { get; set; } = string.Empty;
+        
+
+
+        public virtual ICollection<BlogPost> BlogPosts { get; set; } 
+        
+    }
+}
