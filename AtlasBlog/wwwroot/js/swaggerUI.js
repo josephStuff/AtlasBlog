528c1933477725903382c0b9e79aba0eda04372c
josephStuff <mrwatsonjoe28@yahoo.com>
2022-03-25 15:37:27 -0400

    commented out RoutePrefix

---
+++ AtlasBlog/wwwroot/js/swaggerUI.js
@@ -0,0 +1,9 @@
+﻿
+(function () {
+    window.addEventListener("load", function () {
+        setTimeout(function () {
+            var logo = document.getElementsByClassName('link');
+            logo[0].href = "/Home/Index";
+        });
+    });
+})();
